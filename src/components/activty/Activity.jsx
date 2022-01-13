34ae32da11a9293fb30c122d47260eeb612b7341
moehzi <faizi1234098@gmail.com>
2022-01-13 22:48:19 +0800

    style: change h3 font

--- src/components/activty/Activity.jsx
+++ src/components/activty/Activity.jsx
@@ -3,10 +3,10 @@ import Avatar from "react-avatar";
 
 function Activity(props) {
   return (
-    <div className="flex gap-2 px-2 py-3 bg-card-activty rounded-[12px]">
+    <div className="flex gap-2 px-3 py-4 bg-card-activty rounded-[12px]">
       <Avatar name={props.name} size="40" className="rounded-full" />
       <div className="flex-col gap-1 text-white">
-        <h5 className="text-sm font-medium">{props.name}</h5>
+        <h5 className="font-medium">{props.name}</h5>
         <p className="text-xs opacity-70">{props.message}</p>
       </div>
     </div>
